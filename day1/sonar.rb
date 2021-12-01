326d51b2414f27902c4c2ea4588b70086006da6e
Kylie Stradley <4666485+KyFaSt@users.noreply.github.com>
2021-12-01 16:05:43 +0000

    :sparkles: refactoring

--- day1/sonar.rb
+++ day1/sonar.rb
@@ -1,15 +1,16 @@
 require 'pry'
-values = File.readlines("./input.txt")
 
-values.collect! &:to_i
+def depther(input)
+  values = File.readlines(input).map(&:to_i)
 
-
-total = 0
-values.each_with_index do |i,k|
-  next if values[k-1].nil?
-  if (i > values[k-1])
-    total += 1
+  total = 0
+  values.each_with_index do |i,k|
+    next if values[k-1].nil?
+    if (i > values[k-1])
+      total += 1
+    end
   end
+  puts total
 end
 
-puts total
+depther("./input.txt")
