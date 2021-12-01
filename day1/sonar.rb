37e0350a5dd9291f280020221146cad378f114b7
Kylie Stradley <4666485+KyFaSt@users.noreply.github.com>
2021-12-01 17:33:01 +0000

    day1 pt2

--- day1/sonar.rb
+++ day1/sonar.rb
@@ -13,4 +13,24 @@ def depther(input)
   puts total
 end
 
-depther("./input.txt")
+def window_depther(input)
+  # current = myself plus the next two
+  # is current greater than prev?
+  # if so plus 1 to total
+  # set prev to current
+  values = File.readlines(input).map(&:to_i)
+
+  prev = nil
+  current = 0
+  total = 0
+  values.each_with_index do |i,k|
+    current = i + values[k+1].to_i + values[k+2].to_i
+    if (prev!=nil && current > prev)
+      total += 1
+    end
+    prev = current
+  end
+  puts total
+end
+
+window_depther("./input.txt")
