4231ff05d13df801417284c9fe80a89a2de9ced4
Kylie Stradley <4666485+KyFaSt@users.noreply.github.com>
2021-12-04 02:36:34 +0000

    day 3 pt1

---
+++ day3/diagnostic.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Diagnostic
+  def initialize(input)
+    @values = File.readlines(input)
+    @gamma = ""
+    @epsilon = ""
+    @counter = 0
+  end
+
+  def gamma_counter
+    puts @values[0].chomp.length
+    @values[0].chomp.length.times do |i|
+      binary_abacus = {0 => 0, 1 =>0}
+      @values.each do |value|
+        @counter += 1
+        if value[i] == "0"
+          binary_abacus[0] += 1
+        else
+          binary_abacus[1] += 1
+        end
+      end
+      max = binary_abacus.max { |a, b| a[1] <=> b[1] }[0]
+      @gamma += max.to_s
+      min = binary_abacus.min { |a, b| a[1] <=> b[1] }[0]
+      @epsilon += min.to_s
+    end
+    puts @gamma.to_i(2) * @epsilon.to_i(2)
+  end
+
+end
+
+
+Diagnostic.new('./input.txt').gamma_counter
