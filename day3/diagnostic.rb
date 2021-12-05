3b17305884b5afc6ab679902f45d2db9cd204bd9
Kylie Stradley <4666485+KyFaSt@users.noreply.github.com>
2021-12-05 16:45:03 +0000

    day 3 part 2

--- day3/diagnostic.rb
+++ day3/diagnostic.rb
@@ -3,16 +3,20 @@ require 'pry'
 class Diagnostic
   def initialize(input)
     @values = File.readlines(input)
-    @oxygen_generator = gamma_counter()
-    @epsilon = ""
+    @oxygen_generator = gamma_counter("max")
+    @co2 = gamma_counter("min")
 
     puts "oxygen generator: " + @oxygen_generator.to_s
+    puts "co2 scrubber rating: " + @co2.to_s
+
+    puts "life support rating: " + (@oxygen_generator * @co2).to_s
   end
 
-  def gamma_counter
-    @values[0].chomp.length.times do |i|
+  def gamma_counter(input)
+    values = @values
+    values[0].chomp.length.times do |i|
       binary_abacus = {0 => {count: 0, items: []}, 1 => {count: 0, items: []}}
-      @values.each do |value|
+      values.each do |value|
         if value[i] == "0"
           binary_abacus[0][:count] += 1
           binary_abacus[0][:items] << value
@@ -21,12 +25,20 @@ class Diagnostic
           binary_abacus[1][:items] << value
         end
       end
-      max = binary_abacus.max { |a, b| comparator(a,b) }[0]
-      @values = binary_abacus[max][:items]
+
+      if input == "max"
+        max = binary_abacus.max { |a, b| comparator(a,b) }[0]
+        values = binary_abacus[max][:items]
+      else input == "min"
+        min = binary_abacus.min { |a, b| comparator(a,b) }[0]
+        values = binary_abacus[min][:items]
+      end
+
+      if values.length == 1
+        # this should be the last remaining value in the array
+        return values[0].to_i(2)
+      end
     end
-
-    @values[0].to_i(2)
-
   end
 
   def comparator(a, b)
@@ -42,4 +54,4 @@ end
 # answer to part 1 was 841526
 # if you wrote tests, you wouldn't need comments like this
 
-Diagnostic.new('./test.txt').gamma_counter
+Diagnostic.new('./input.txt')
