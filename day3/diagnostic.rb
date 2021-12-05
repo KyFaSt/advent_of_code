beb5b621d7fa68e4f2cec77bbb3af2d9f418e781
Kylie Stradley <4666485+KyFaSt@users.noreply.github.com>
2021-12-05 16:17:41 +0000

    oxygen generator

--- day3/diagnostic.rb
+++ day3/diagnostic.rb
@@ -9,23 +9,36 @@ class Diagnostic
 
   def gamma_counter
     @values[0].chomp.length.times do |i|
-      binary_abacus = {0 => 0, 1 =>0}
+      binary_abacus = {0 => {count: 0, items: []}, 1 => {count: 0, items: []}}
       @values.each do |value|
         if value[i] == "0"
-          binary_abacus[0] += 1
+          binary_abacus[0][:count] += 1
+          binary_abacus[0][:items] << value
         else
-          binary_abacus[1] += 1
+          binary_abacus[1][:count] += 1
+          binary_abacus[1][:items] << value
         end
       end
-      max = binary_abacus.max { |a, b| a[1] <=> b[1] }[0]
-      @gamma += max.to_s
-      min = binary_abacus.min { |a, b| a[1] <=> b[1] }[0]
-      @epsilon += min.to_s
+      max = binary_abacus.max { |a, b| comparator(a,b) }[0]
+      @values = binary_abacus[max][:items]
     end
-    puts @gamma.to_i(2) * @epsilon.to_i(2)
+
+    puts "oxygen generator: " + @values[0].to_i(2).to_s
   end
+
+  def comparator(a, b)
+    if a[1][:count] == b[1][:count]
+      a[0]
+    else
+      a[1][:count] <=> b[1][:count]
+    end
+  end
+
 
 end
 
 
-Diagnostic.new('./input.txt').gamma_counter
+# answer to part 1 was 841526
+# if you wrote tests, you wouldn't need comments like this
+
+Diagnostic.new('./test.txt').gamma_counter
